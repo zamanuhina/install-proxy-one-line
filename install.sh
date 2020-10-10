db59fe1a819c7d62f5404232855161a8882f055d
zamanuhina <72570524+zamanuhina@users.noreply.github.com>
2020-10-11 00:31:28 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+CHECK_OS="$(awk '/^ID=/' /etc/*os-release | awk -F'=' '{ print tolower($2) }')"
+
+if [ "${CHECK_OS}" = "debian" ] || [ "${CHECK_OS}" = "\"debian\"" ] || [ "${CHECK_OS}" = "ubuntu" ] || [ "${CHECK_OS}" = "\"ubuntu\"" ]; then
+    DEBIAN_FRONTEND=noninteractive apt-get -y -qq update
+    DEBIAN_FRONTEND=noninteractive apt-get -y -qq install apache2-utils squid3 wget
+elif [ "${CHECK_OS}" = "fedora" ] || [ "${CHECK_OS}" = "\"fedora\"" ]; then
+    dnf -y install httpd-tools squid3 wget
+elif [ "${CHECK_OS}" = "centos" ] || [ "${CHECK_OS}" = "\"centos\"" ]; then
+    yum install -y httpd-tools squid3 wget
+fi
+
+PROXY_USER="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)"
+PROXY_PASS="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)"
+PROXY_IP="$(wget http://ipecho.net/plain -O - -q)"
+PROXY_PORT="5$(cat /dev/urandom | tr -dc '0-9' | fold -w 256 | head -n 1 | head --bytes 1)$(cat /dev/urandom | tr -dc '0-9' | fold -w 256 | head -n 1 | head --bytes 1)$(cat /dev/urandom | tr -dc '0-9' | fold -w 256 | head -n 1 | head --bytes 1)$(cat /dev/urandom | tr -dc '0-9' | fold -w 256 | head -n 1 | head --bytes 1)"
+
+echo "${PROXY_PASS}" | htpasswd -c -i /etc/squid3/passwd "${PROXY_USER}"
+
+cat <<EOT >> /etc/squid3/squid.conf
+    http_port ${PROXY_PORT}
+    auth_param basic program /usr/lib/squid3/basic_ncsa_auth /etc/squid3/passwd
+    auth_param basic children 5
+    auth_param basic realm Squid Basic Authentication
+    auth_param basic credentialsttl 2 hours
+    acl auth_users proxy_auth REQUIRED
+    request_header_access X-Forwarded-For deny all
+    request_header_access Via deny all
+    request_header_access Proxy deny all
+    request_header_access Cache-Control deny all
+    http_access allow auth_users
+EOT
+
+systemctl start squid3
+systemctl enable squid3
+systemctl restart squid3
+
+echo "YOUR PROXY - ${PROXY_USER}:${PROXY_PASS}@${PROXY_IP}:${PROXY_PORT}"
